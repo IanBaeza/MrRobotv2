b837406bf19c8b3036454522a8223125a05601cc
IanBaeza <inibr2@gmail.com>
2019-12-05 21:33:45 -0300

    Proyecto Tercera Parte

--- mrroboton/admin.py
+++ mrroboton/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import Gabinete, Fuente, PlacaMadre, Ram, Procesador, TarjetaVideo, Almacenamiento, Computador
+from . models import Computador
 
-admin.site.register(Gabinete)
-admin.site.register(Fuente)
-admin.site.register(PlacaMadre)
-admin.site.register(Ram)
-admin.site.register(Procesador)
-admin.site.register(TarjetaVideo)
-admin.site.register(Almacenamiento)
 admin.site.register(Computador)
--- mrroboton/models.py
+++ mrroboton/models.py
@@ -4,70 +4,14 @@ import uuid
 
 # Create your models here.
 
-class Gabinete(models.Model):
-    modelogabinete=models.CharField(max_length=30)
-    tamanogabinete=models.CharField(max_length=5)
-    marcagabinete=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.modelogabinete},{self.tamanogabinete},{self.marcagabinete}'
-
-class Fuente(models.Model):
-    modelofuente=models.CharField(max_length=30)
-    marcafuente=models.CharField(max_length=30)
-    wattsfuente=models.CharField(max_length=10)
-
-    def __str__(self):
-        return f'{self.modelofuente},{self.marcafuente},{self.wattsfuente}'
-
-class PlacaMadre(models.Model):
-    marcaplaca=models.CharField(max_length=30)
-    modeloplaca=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.marcaplaca},{self.modeloplaca}'
-
-class Ram(models.Model):
-    marcaram=models.CharField(max_length=30)
-    modeloram=models.CharField(max_length=30)
-    memoriaram=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.marcaram},{self.modeloram},{self.memoriaram}'
-
-class Procesador(models.Model):
-    generacionproce=models.CharField(max_length=30)
-    modeloproce=models.CharField(max_length=30)
-    marcaproce=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.generacionproce},{self.modeloproce},{self.marcaproce}'
-
-class TarjetaVideo(models.Model):
-    modelovideo=models.CharField(max_length=30)
-    marcavideo=models.CharField(max_length=30)
-    generacionvideo=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.modelovideo},{self.marcavideo},{self.generacionvideo}'
-
-
-class Almacenamiento(models.Model):
-    modeloalma=models.CharField(max_length=30)
-    capacidadalma=models.CharField(max_length=10)
-    tipoalma=models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.modeloalma},{self.capacidadalma},{self.tipoalma}'
-
 class Computador(models.Model):
-    Gabinete=models.ForeignKey('Gabinete',on_delete=models.SET_NULL, null=True)
-    Fuente=models.ForeignKey('Fuente',on_delete=models.SET_NULL, null=True)
-    PlacaMadre=models.ForeignKey('PlacaMadre',on_delete=models.SET_NULL, null=True)
-    Ram=models.ForeignKey('Ram',on_delete=models.SET_NULL, null=True)
-    Procesador=models.ForeignKey('Procesador',on_delete=models.SET_NULL, null=True)
-    TarjetaVideo=models.ForeignKey('TarjetaVideo',on_delete=models.SET_NULL, null=True)
-    Almacenamiento=models.ForeignKey('Almacenamiento',on_delete=models.SET_NULL, null=True)
+    Gabinete=models.CharField(max_length=100)
+    Fuente=models.CharField(max_length=100)
+    PlacaMadre=models.CharField(max_length=100)
+    Ram=models.CharField(max_length=100)
+    Procesador=models.CharField(max_length=100)
+    TarjetaVideo=models.CharField(max_length=100)
+    Almacenamiento=models.CharField(max_length=100)
 
     def __str__(self):
-    	return f'{self.Gabinete},{self.Fuente},{self.PlacaMadre},{self.Ram},{self.Procesador},{self.TarjetaVideo},{self.Almacenamiento}'
+    	return self.Gabinete
